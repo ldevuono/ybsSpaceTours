290ad356aa1221353b02dcb969e3df61503a4b78
lauradevuono <lauradevuono@gmail.com>
2022-12-11 11:55:21 -0500

    started to build out App.js, first API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+
+
 function App() {
+
+  const tripsArray = [
+    "mercury", "venus", "uranus", "mars", "jupiter", "saturn"
+  ]
+
+  const resArray = []
+
+  useEffect(() => {
+    tripsArray.forEach((trip) => {
+      axios({
+        url: "https://images-api.nasa.gov/search",
+        method: "GET",
+        dataResponse: "json",
+        params: {
+          q: trip
+        }
+      }).then((response) => {
+        // console.log(response.data);
+        resArray.push(response.data);
+      });
+    })
+    console.log(resArray)
+  }, []);
+
+
+
   return (
     <div className="App">
       <h1>Welcome to our super cool project!!</h1>
