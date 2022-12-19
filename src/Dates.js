f59278bbc38206a31ce459fab8ea67e63059faba
Andres Plana <dudeandu@hotmail.com>
2022-12-19 13:31:12 -0500

    solved the date/diameter error

--- src/Dates.js
+++ src/Dates.js
@@ -7,16 +7,18 @@ function Dates() {
 	const [dateList, setDateList] = useState([]);
 	const [dates, setDates] = useState(false);
 
+	const totalDates = 6;
+
 
 	// Date.prototype.addDays = function (days) {
-	// 	var date = new Date(this.valueOf());
+	// 	let date = new Date(this.valueOf());
 	// 	date.setDate(date.getDate() + days);
 	// 	return date;
 	// }
 
 	// const getDates = (startDate, stopDate) => {
-	// 	var dateArray = new Array();
-	// 	var currentDate = startDate;
+	// 	let dateArray = new Array();
+	// 	let currentDate = startDate;
 	// 	while (currentDate <= stopDate) {
 	// 		dateArray.push(new Date(currentDate));
 	// 		currentDate = currentDate.addDays(1);
@@ -31,7 +33,7 @@ function Dates() {
 	useEffect(() => {
 		// eslint-disable-next-line
 		Date.prototype.addDays = function (days) {
-			var date = new Date(this.valueOf());
+			let date = new Date(this.valueOf());
 			date.setDate(date.getDate() + days);
 			return date;
 		}
@@ -48,7 +50,7 @@ function Dates() {
 		}
 
 		const todayDate = new Date();
-		setDateList(getDates(todayDate, (todayDate).addDays(30)));
+		setDateList(getDates(todayDate, (todayDate).addDays(totalDates)));
 		
 
 		// console.log(dateList);
@@ -62,6 +64,7 @@ function Dates() {
 				api_key: "0vfR0cK5U5L4Afnbrb20dF1VAFDSQIm1KDZnbJ5g",
 				// api_key: "JDRuKCmCHys7mfyiaqaUomCeZqsgjnv8iQyTjl8l",
 				start_date: `${todayDate.getFullYear()}-${("0" + (todayDate.getMonth() + 1)).slice(-2)}-${("0" + todayDate.getDate()).slice(-2)}`
+				// end_date: `${(todayDate).addDays(totalDates).getFullYear()}-${("0" + ((todayDate).addDays(totalDates).getMonth() + 1)).slice(-2)}-${("0" + (todayDate).addDays(totalDates).getDate()).slice(-2)}`
 			}
 		}).then((response) => {
 			console.log(response);
@@ -75,20 +78,28 @@ function Dates() {
 				hazardousObjects[object].forEach((d) => {
 					// console.log(d.estimated_diameter.kilometers.estimated_diameter_max)
 					// console.log(d);
-					if (d.estimated_diameter.kilometers.estimated_diameter_max >= 1.5) {
+					if (d.estimated_diameter.kilometers.estimated_diameter_max >= .5) {
 						// setTravelDays([...travelDays, object])
 						tempArray.push(object);
 					}
 				})
 			}
 			tempArray = [...new Set(tempArray)];
-
-
+			console.log(tempArray)
+			tempArray.sort(function(a,b) {
+				a = a.split('-').reverse().join('');
+				b = b.split('-').reverse().join('');
+				return a > b ? 1 : a < b ? -1 : 0;
+				// return a.localeCompare(b);         // <-- alternative 
+			});
+			console.log(tempArray)
+			
+			
 			let allDatesObject = []
-			dateList.forEach((date) => {
-				const thisDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-				tempArray.forEach((d) => {
-					if (d === thisDate) {
+			dateList.forEach((date, i) => {
+				const thisDate = `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}`;
+
+					if (thisDate == tempArray[i]) {
 						allDatesObject.push({
 							date: thisDate,
 							isItSafe: false
@@ -99,13 +110,13 @@ function Dates() {
 							isItSafe: true
 						})
 					}
-				})
+				// })
 			})
 			allDatesObject = [...new Set(allDatesObject)];
 			// console.log(tempArray);
 			// console.log(response.data.collection.items[0].links[0].href);
 			setDateResp(allDatesObject);
-			console.log(dateResp);
+			// console.log(dateResp);
 		});
 	// eslint-disable-next-line
 	}, [dates]);
@@ -139,7 +150,7 @@ function Dates() {
 				<label htmlFor="email" className="sr-only">Email Address</label>
 				<input type="email" name="email" id="email" placeholder="Email Address" />
 				<div className="getDates">
-					<button onClick={() => setDates(!dates)} aria-label="Click here to see list of available dates">See available dates</button>
+					<button onClick={() => setDates(true)} aria-label="Click here to see list of available dates">See available dates</button>
 				</div>
 				<label htmlFor="dates" className="sr-only">Choose a date</label>
 				<select
@@ -158,7 +169,10 @@ function Dates() {
 							<option 
 							key={`dropdownDate${date.date}`}
 							disabled={date.isItSafe ? false : true} 
-							value={date.date}>{date.date}</option>
+							value={date.date}
+							>
+								{date.date}
+							</option>
 						)
 					})}
 					{/* <option value="">Pick one:</option> */}
